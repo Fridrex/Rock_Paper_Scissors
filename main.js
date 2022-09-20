441590a61fa94d5c54a08838cc729f574b994563
Peter <fridrex@mail.com>
2022-09-20 17:58:44 +0100

    created PC choice function

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 function random(number) {
     return Math.floor(Math.random() * number);
   }
+
+function getComputerChoice() {
+    let option = random(3);
+
+    switch (option) {
+        case 0:
+            return 'Rock'
+            break;
+        case 1:
+            return 'Paper'
+            break;
+        case 2:
+            return 'Scissors'
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(getComputerChoice())
