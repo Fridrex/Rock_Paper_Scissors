7b2c57d67c392b85d306706bb6457670ff37557f
Peter <fridrex@mail.com>
2022-09-22 19:47:15 +0100

    created SELECTIONS

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const selectionButtons = document.querySelectorAll('[data-selection]');
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', (e) => {
         const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
         makeSelection(selectionName)
     })
 })
